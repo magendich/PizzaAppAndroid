6e30e33f31611fb4196af4c578006af261677c7e
Magomed Allahverdiev <magomed.allahverdiev@kupibilet.ru>
2023-09-06 19:39:55 +0300

    Добавил файл PizzaDto

--- app/src/main/java/com/example/pizzaapp/data/ApiRequestService.kt
+++ app/src/main/java/com/example/pizzaapp/data/ApiRequestService.kt
@@ -1,9 +1,9 @@
 package com.example.pizzaapp.data
 
-import com.example.pizzaapp.data.mapper.PizzaDto
+import com.example.pizzaapp.data.mapper.ProductDto
 import retrofit2.http.GET
 
 interface ApiRequestService {
     @GET("pizzatest.php")
-    suspend fun getPizzas(): List<PizzaDto>
+    suspend fun getPizzas(): List<ProductDto>
 }
