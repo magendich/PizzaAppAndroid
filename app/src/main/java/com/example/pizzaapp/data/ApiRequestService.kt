73bc2c68d5f46aad151e351dce84807354740a2d
Magomed Allahverdiev <magomed.allahverdiev@kupibilet.ru>
2023-09-06 20:01:04 +0300

    Добавитл правки

--- app/src/main/java/com/example/pizzaapp/data/ApiRequestService.kt
+++ app/src/main/java/com/example/pizzaapp/data/ApiRequestService.kt
@@ -1,9 +1,9 @@
 package com.example.pizzaapp.data
 
-import com.example.pizzaapp.data.mapper.ProductDto
+import com.example.pizzaapp.data.mapper.PizzaDto
 import retrofit2.http.GET
 
 interface ApiRequestService {
     @GET("pizzatest.php")
-    suspend fun getPizzas(): List<ProductDto>
+    suspend fun getPizzas(): List<PizzaDto>
 }
